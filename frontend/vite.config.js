69a949d3f02c813abf91937f01edb2d552dd4df4
ananta-devs <anantahazra.bdn@gmail.com>
2025-12-25 16:09:37 +0530

    added new target

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://linkify-2v6p.onrender.com',
         changeOrigin: true,
         secure: false,
       },
